17aae4ae6b7c80666b8dd0f69e489a1908d65cd0
JackSelleck <jackguyselleck@gmail.com>
2026-03-06 16:04:55 +0000

    Got some basic gull flying

--- Assets/Scripts/SeagullController.cs
+++ Assets/Scripts/SeagullController.cs
@@ -8,17 +8,25 @@ public class SeagullController : MonoBehaviour
     [SerializeField] private Animator anim;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private CapsuleCollider col;
-    [Header("Movement")]
+    [Space]
+    [Header("Flight Tweaks")]
     [SerializeField] private float smoothTime = 0.2f;
+    [SerializeField] private float flyforce = 10;
+    [SerializeField] private float glideforce = 8;
+    [SerializeField] private float pitchSpeed = 8;
+    [SerializeField] private float turnSpeed = 8;
+    [SerializeField] private float maxBankAngle = 8;
+    [Space]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [Space]
     [SerializeField] private float groundedGravity = 2f;
     [SerializeField] private float flyingGravity = 0.5f;
     [SerializeField] private float glidingGravity = 0.2f;
-    [SerializeField] private float walkSpeed = 5f;
-    [SerializeField] private float jumpForce = 5f;
 
     private InputSystem_Actions controls;
 
-    private bool isGrounded = false;
+    private bool isGrounded = true;
     private bool isGliding = false;
     private Vector2 moveInput;
     private Vector3 currentVelocityRef; 
@@ -39,45 +47,73 @@ public class SeagullController : MonoBehaviour
 
     void FixedUpdate()
     {
-        ApplyGravity();
-            
+        ApplyAmbientForces();
+        Vector3 velocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, rb.linearVelocity.z);
+        anim.SetFloat("Speed", velocity.magnitude);
+
         moveInput = controls.Player.Move.ReadValue<Vector2>();
 
-        if (moveInput != Vector2.zero)
+        if (isGrounded)
         {
-            Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
-            rb.linearVelocity = Vector3.SmoothDamp(
-                rb.linearVelocity,
-                new Vector3(movement.x, rb.linearVelocity.y, movement.z),
-                ref currentVelocityRef,
-                smoothTime
-            ); rb.constraints = RigidbodyConstraints.FreezeRotationX| RigidbodyConstraints.FreezeRotationZ;
-            anim.SetBool("Moving", true);
-            Debug.Log("Moving");
+            Vector3 movement = new Vector3(-moveInput.x, 0, -moveInput.y) * walkSpeed;
 
-            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(-moveInput.x, 0, -moveInput.y));
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+            rb.linearVelocity = Vector3.SmoothDamp(
+                 rb.linearVelocity,
+                 new Vector3(movement.x, rb.linearVelocity.y, movement.z),
+                 ref currentVelocityRef,
+                 smoothTime
+            );
+            if (movement.magnitude > 0.1f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(new Vector3(movement.x, 0, movement.z));
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+            }
+
+            anim.SetBool("Moving", movement.magnitude > 0.1f);
         }
-        else if (isGrounded)
+        else 
         {
-            rb.linearVelocity = new Vector3(0, 0, 0);
-            anim.SetBool("Moving", false);
+            float pitch = moveInput.y * pitchSpeed * Time.deltaTime;
+            float yaw = moveInput.x * turnSpeed * Time.deltaTime;
+
+            transform.Rotate(pitch, yaw, 0, Space.Self);
+
+            if (isGliding)
+            {
+                rb.AddRelativeForce(Vector3.forward * glideforce, ForceMode.Acceleration);
+                rb.AddForce(Vector3.down * glidingGravity, ForceMode.Acceleration);
+                Debug.Log("Flying");
+            }
+            else if (!isGliding)
+            {
+                rb.AddRelativeForce(Vector3.forward * flyforce, ForceMode.Acceleration);
+                rb.AddForce(Vector3.down * flyingGravity, ForceMode.Acceleration);
+                anim.SetBool("Gliding", true);
+                Debug.Log("Gliding");
+            }
+
+            // Gradually resets the Z-rotation so the gull dont get stuck upside down
+            float rollAngle = -moveInput.x * maxBankAngle; // Visual banking
+            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, rollAngle);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
+
+            rb.constraints = RigidbodyConstraints.None; 
+            rb.linearDamping = 4f;       
         }
+
+        if (moveInput == Vector2.zero)
+        {
+            anim.SetBool("Idle", true);
+        }
+        else {anim.SetBool("Idle", false);}
     }
 
-    private void ApplyGravity()
+    private void ApplyAmbientForces()
     {
-        if (!isGrounded && !isGliding)
-        {
-            rb.AddForce(Vector3.down * flyingGravity, ForceMode.Acceleration);
-        }
-        else if (isGliding)
-        {
-            rb.AddForce(Vector3.down * glidingGravity, ForceMode.Acceleration);
-        }
-        else if (isGrounded)
+        if (isGrounded)
         {
             rb.AddForce(Vector3.down * groundedGravity, ForceMode.Acceleration);
+            Debug.Log("Grounded");
         }
     }
 
@@ -90,6 +126,7 @@ public class SeagullController : MonoBehaviour
         if (context.interaction is TapInteraction)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            rb.AddRelativeForce(Vector3.forward * flyforce * 2, ForceMode.Acceleration);
         }
         if (context.interaction is HoldInteraction)
         {
@@ -109,14 +146,14 @@ public class SeagullController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         isGrounded = true;
         anim.SetBool("Grounded", true);
     }
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        isGrounded = true;
+        isGrounded = false;
         anim.SetBool("Grounded", false);
     }
 
